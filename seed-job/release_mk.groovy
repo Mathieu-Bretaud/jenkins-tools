da13787e420c1d2334b98700a503d323afc1d55e
Mathieu Bretaud <mathieu_bretaud@carrefour.com>
2017-07-12 18:50:47 +0200

    edit project

--- seed-job/release_mk.groovy
+++ seed-job/release_mk.groovy
@@ -12,9 +12,13 @@ job(jobName) {
         stringParameterDefinition {
             name("GIT_BRANCH")
             defaultValue("develop")
-            description("Git branch to release.")
+            description("Git branch with modifications.")
         }
-        choiceParam('RELEASE_TYPE', ['Stable', 'M', 'RC'], 'Stable release \nMilestone  \nRelease Candidate')
+        stringParameterDefinition {
+            name("GIT_BRANCH_TO_PUSH")
+            defaultValue("staging")
+            description("Git branch to push.")
+        }
     }
     environmentVariables {
         env('DISABLE_DATASCIENCE_DEV',"true")
@@ -37,7 +41,7 @@ job(jobName) {
         }
     }
     wrappers {
-        buildName('${GIT_PROJECT} ${GIT_BRANCH} ${RELEASE_TYPE} #${BUILD_NUMBER}')
+        buildName('${GIT_PROJECT} ${GIT_BRANCH} ${GIT_BRANCH_TO_PUSH}')
     }
     steps {
         shell('/usr/bin/git config --global user.email "mathieu_bretaud@carrefour.com"\n' +
@@ -45,31 +49,14 @@ job(jobName) {
                 '\n' +
                 '/usr/bin/git checkout ${GIT_BRANCH}\n' +
                 '\n' +
-                'echo \'BUILD_VERSION=0.0.1\' > version.mk' +
-                '\n' +
-                '/usr/bin/git branch --set-upstream ${GIT_BRANCH} origin/${GIT_BRANCH}\n' +
-                '\n' +
-                'export ACTUAL_VERSION=0.0.1\n' +
-                '\n' +
                 'export NEXT_VERSION=0.0.2\n' +
                 'export RELEASE_VERSION=0.0.2\n' +
                 '\n' +
                 '/usr/bin/git commit -am "prepare for release $RELEASE_VERSION"\n' +
                 '\n' +
-                'export http_proxy=${DATASCIENCE_HTTP_PROXY}\n' +
-                'export https_proxy=${DATASCIENCE_HTTPS_PROXY}\n' +
-                'export ftp_proxy=${DATASCIENCE_FTP_PROXY}\n' +
-                'export no_proxy=${DATASCIENCE_NO_PROXY}\n' +
-                '\n' +
                 'export TAG_VERSION="v$RELEASE_VERSION"\n' +
                 '\n' +
-                '/usr/bin/git checkout -b release/$TAG_VERSION\n' +
                 '/usr/bin/git tag $TAG_VERSION\n' +
-                '/usr/bin/git push -uf origin $TAG_VERSION\n' +
-                '\n' +
-                '/usr/bin/git checkout ${GIT_BRANCH}\n' +
-                '/usr/bin/git commit -am "Setting version to $NEXT_VERSION"\n' +
-                '\n' +
-                '/usr/bin/git push -uf origin $GIT_BRANCH')
+                '/usr/bin/git push origin ${GIT_BRANCH}:${GIT_BRANCH_TO_PUSH}\n')
     }
 }
